1382af01d8dbab61a56ce2f9676610f9a492ab93
Gautam Mehta <gautam.mehta@rtcamp.com>
2026-06-11 18:47:28 +0530

    feat: create user and base entity

---
+++ src/main/java/com/coderGtm/delta/user/User.java
@@ -0,0 +1,43 @@
+package com.coderGtm.delta.user;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import com.coderGtm.delta.common.entity.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User extends BaseEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private UUID id;
+
+	@Column(unique = true)
+	private String firebaseUid;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column(unique = true)
+	private String email;
+
+	private String phone;
+
+	private Instant deletedAt;
+}
